5a225005aad0676ea6dd8075d0613a3feaec6757
Brian Wiltz <bwiltz@scitent.com>
2015-07-31 13:32:04 -0400

    Changes to post_submit.js

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,9 +8,13 @@ Template.postSubmit.events({
 		};
 
 		Meteor.call('postInsert', post, function(error, result) {
-			//display the error to the user and abort
+			// display the error to the user and abort
 			if (error)
 				return alert(error.reason);
+
+			// show this result but route anyway
+			if (results.postExists)
+				alert('This link has already been posted');
 
 			Router.go('postPage', {_id: result._id});
 		});
